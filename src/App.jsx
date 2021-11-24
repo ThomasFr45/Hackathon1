0ac30b43c715a8dae1d609eb1c56d3e60d1041e8
thomas Fran <thomasfran1935@icloud.com>
2021-11-24 18:53:57 +0100

    Base de Hackathon

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Banniere from "./components/Banniere";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="page-container">
+      <div className="content-wrap">
+        <Router>
+          <Navbar />
+          <Banniere title="Premier Hackathon avec la team plop !" />
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Router>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
