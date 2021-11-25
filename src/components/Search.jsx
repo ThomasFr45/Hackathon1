ca983172d0e9142b1a5ffac6a465490fa62a9cb4
Aphideth <aphideth.dev@gmail.com>
2021-11-25 14:30:54 +0100

    add fav songs

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -12,6 +12,7 @@ const USERS = [
     age: 20,
     picture: "https://randomuser.me/api/portraits/men/29.jpg",
     music: "jazz , blues",
+    fav: "Coldplay | Adventure of a Lifetime  &  Adele | Rolling in the Deep ",
   },
   {
     id: 2,
@@ -20,6 +21,7 @@ const USERS = [
     age: 24,
     picture: "https://randomuser.me/api/portraits/men/30.jpg",
     music: "hiphop , rap",
+    fav: "Beyoncé | Single Ladies  &  Coldplay | Adventure of a Lifetime ",
   },
   {
     id: 3,
@@ -28,6 +30,7 @@ const USERS = [
     age: 27,
     picture: "https://randomuser.me/api/portraits/men/31.jpg",
     music: "raggae , funk",
+    fav: " Bruno Mars | Uptown Funk & Lizzo | Good as Hell ",
   },
   {
     id: 4,
@@ -36,6 +39,7 @@ const USERS = [
     age: 30,
     picture: "https://randomuser.me/api/portraits/men/32.jpg",
     music: "pop , rock",
+    fav: " Katy Perry | Firework & The Pixies | Where Is My Mind? ",
   },
   {
     id: 5,
@@ -44,6 +48,7 @@ const USERS = [
     age: 32,
     picture: "https://randomuser.me/api/portraits/men/33.jpg",
     music: "raggae , jazz",
+    fav: " Pharrell Williams | Happy & Miles Davis | All Blues ",
   },
   {
     id: 6,
@@ -52,6 +57,7 @@ const USERS = [
     age: 35,
     picture: "https://randomuser.me/api/portraits/men/34.jpg",
     music: "classical , electronic",
+    fav: " Daft Punk | Get Lucky & Miles Davis | So What ",
   },
   {
     id: 7,
@@ -60,6 +66,7 @@ const USERS = [
     age: 37,
     picture: "https://randomuser.me/api/portraits/men/35.jpg",
     music: "electronic , rock",
+    fav: " Martin Solveig | The Night Out & DJ Snake | Turn Down for What ",
   },
   {
     id: 8,
@@ -68,6 +75,7 @@ const USERS = [
     age: 40,
     picture: "https://randomuser.me/api/portraits/men/36.jpg",
     music: "funk , latin",
+    fav: " Marvin Gaye | Got To Give It Up & James Brown | Papas Got A Brand New Bag ",
   },
   {
     id: 9,
@@ -76,6 +84,7 @@ const USERS = [
     age: 45,
     picture: "https://randomuser.me/api/portraits/men/37.jpg",
     music: "soul , country",
+    fav: " Amy Winehouse | ReHab & Brad Paisley | Welcome to the Future ",
   },
   {
     id: 10,
@@ -84,6 +93,7 @@ const USERS = [
     age: 70,
     picture: "https://randomuser.me/api/portraits/men/38.jpg",
     music: "blues , rock",
+    fav: " Duke Ellington | Take The A Train & Jimi Hendrix | Purple Haze ",
   },
   {
     id: 11,
@@ -92,6 +102,7 @@ const USERS = [
     age: 20,
     picture: "https://randomuser.me/api/portraits/women/30.jpg",
     music: "hiphop , rock",
+    fav: " Outkast | Hey Ya! & Led Zeppelin | Whole Lotta Love ",
   },
   {
     id: 12,
@@ -100,6 +111,7 @@ const USERS = [
     age: 25,
     picture: "https://randomuser.me/api/portraits/women/31.jpg",
     music: "funk , rock",
+    fav: " The Weeknd | Blinding Lights & Destiny's Child | Bootylicious ",
   },
   {
     id: 13,
@@ -108,6 +120,7 @@ const USERS = [
     age: 30,
     picture: "https://randomuser.me/api/portraits/women/32.jpg",
     music: "blues , pop",
+    fav: " Lady Gaga | Poker Face & Billie Eilish | Bad Guy ",
   },
   {
     id: 14,
@@ -116,6 +129,7 @@ const USERS = [
     age: 35,
     picture: "https://randomuser.me/api/portraits/women/33.jpg",
     music: "latin , jazz",
+    fav: " Shakira | Hips Don't Lie & John Coltrane | My Favorite Things ",
   },
   {
     id: 15,
@@ -124,6 +138,7 @@ const USERS = [
     age: 37,
     picture: "https://randomuser.me/api/portraits/women/34.jpg",
     music: "electronic , country",
+    fav: " David Guetta | Titanium & Duck Sauce | Barbra Streisand ",
   },
   {
     id: 16,
@@ -132,6 +147,7 @@ const USERS = [
     age: 40,
     picture: "https://randomuser.me/api/portraits/women/35.jpg",
     music: "pop , rock",
+    fav: " Justin Timberlake | SexyBack  & The Rolling Stones | Sympathy for the Devil ",
   },
   {
     id: 17,
@@ -140,6 +156,7 @@ const USERS = [
     age: 45,
     picture: "https://randomuser.me/api/portraits/women/36.jpg",
     music: "raggae , rock",
+    fav: " Nicki Minaj | Starships & Bob Marley | Bob Marley ",
   },
   {
     id: 18,
@@ -148,6 +165,7 @@ const USERS = [
     age: 49,
     picture: "https://randomuser.me/api/portraits/women/37.jpg",
     music: "pop , latin",
+    fav: " Gwen Stefani | Hollaback Girl & Marc Anthony | Ahora Quien ",
   },
   {
     id: 19,
@@ -156,6 +174,7 @@ const USERS = [
     age: 54,
     picture: "https://randomuser.me/api/portraits/women/38.jpg",
     music: "hiphop , funk",
+    fav: " Eve | Let Me Blow Ya Mind & Otto Knows | Million Voices ",
   },
   {
     id: 20,
@@ -164,6 +183,7 @@ const USERS = [
     age: 32,
     picture: "https://randomuser.me/api/portraits/women/39.jpg",
     music: "classical , jazz",
+    fav: " Lorde | Royals & Gnarls Barkley | Crazy ",
   },
 ];
 
@@ -200,17 +220,17 @@ function Search() {
         {foundUsers && foundUsers.length > 0 ? (
           foundUsers.map((user) => (
             <div className="card">
-      <img src={user.picture} alt='' />
-      <div className='infos'>
-      <p>Pseudo : {user.pseudo}</p>
-      <p>Genre : {user.gender}</p>
-      <p>Age : {user.age}</p>
-      </div>
-      <h4>Genres Favoris :</h4>
-      <div className='genres'>
-        <p>{user.music}</p>
-      </div>
-    </div>
+              <img src={user.picture} alt="" />
+              <div className="infos">
+                <p>Pseudo : {user.pseudo}</p>
+                <p>Genre : {user.gender}</p>
+                <p>Age : {user.age}</p>
+              </div>
+              <h4>Genres Favoris :</h4>
+              <div className="genres">
+                <p>{user.music}</p>
+              </div>
+            </div>
           ))
         ) : (
           <h1>No match found!</h1>
