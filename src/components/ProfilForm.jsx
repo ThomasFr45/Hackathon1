d58f1e2a62169f4a47f21a3f7f0c4d97065b158d
Mathilde <mathildelie@gmail.com>
2021-11-25 15:54:09 +0100

    genre selection in form

--- src/components/ProfilForm.jsx
+++ src/components/ProfilForm.jsx
@@ -5,7 +5,14 @@ export const ProfilForm = () => {
   const [name, setName] = useState();
   const [firstName, setFirstName] = useState();
   const [song, setSong] = useState();
-  const [genres, setGenres] = useState();
+  const [genres, setGenres] = useState([]);
+
+  const handleGenres = (checkedGenre) => {
+    genres.includes(checkedGenre)
+      ? setGenres(genres.filter((item) => item !== checkedGenre))
+      : setGenres([...genres, checkedGenre])
+  }
+  
 
   const sendData = (e) => {
     e.preventDefault();
@@ -27,10 +34,35 @@ export const ProfilForm = () => {
     <label htmlFor="Prénom">Prénom</label><br />
     <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} /><br />
     <label htmlFor="favoriteSong">Vos genres préférés</label><br />
-    <div>
-    <input type="checkbox" value={genres} onChange={(e) => setGenres(e.target.value)} />Rock
-    </div><br />
-    
+    <input type="checkbox" value={genres} onChange={() => handleGenres('rock')} />
+    <label htmlFor="">Rock</label>
+    <input type="checkbox" value={genres} onChange={() => handleGenres('hiphop')} />
+    <label htmlFor="">Hiphop</label>
+    <input type="checkbox" value={genres} onChange={() => handleGenres('rap')} />
+    <label htmlFor="">Rap</label>
+    <input type="checkbox" value={genres} onChange={() => handleGenres('raggae')} />
+    <label htmlFor="">Raggae</label>
+    <br />
+    <input type="checkbox" value={genres} onChange={() => handleGenres('funk')} />
+    <label htmlFor="">Funk</label>
+    <input type="checkbox" value={genres} onChange={() => handleGenres('pop')} />
+    <label htmlFor="">Pop</label>
+    <input type="checkbox" value={genres} onChange={() => handleGenres('jazz')} />
+    <label htmlFor="">Jazz</label>
+    <input type="checkbox" value={genres} onChange={() => handleGenres('electronic')} />
+    <label htmlFor="">Electronic</label>
+    <br />
+    <input type="checkbox" value={genres} onChange={() => handleGenres('classical')} />
+    <label htmlFor="">Classical</label>
+    <input type="checkbox" value={genres} onChange={() => handleGenres('latin')} />
+    <label htmlFor="">Latin</label>
+    <input type="checkbox" value={genres} onChange={() => handleGenres('soul')} />
+    <label htmlFor="">Soul</label>
+    <input type="checkbox" value={genres} onChange={() => handleGenres('country')} />
+    <label htmlFor="">Country</label>
+    <input type="checkbox" value={genres} onChange={() => handleGenres('blues')} />
+    <label htmlFor="">Blues</label>
+    <br />
     <label htmlFor="favoriteSong">Votre chanson du moment:</label><br />
     <input type="text" value={song} onChange={(e) => setSong(e.target.value)} /><br />
     <button type="submit">C'est parti!</button>
--- src/components/Profiles.jsx
+++ src/components/Profiles.jsx
@@ -1,5 +1,6 @@
 export const Profiles = () => {
   const profiles = JSON.parse(localStorage.getItem("formData"));
+  const chosenGenres = profiles.genres
   console.log(profiles);
 
   return (
@@ -7,7 +8,9 @@ export const Profiles = () => {
        
           <p>{profiles.name}</p>
           <p>{profiles.firstName}</p>
-          <p>{profiles.artist}</p>
+          <p>{profiles.song}</p>
+          {chosenGenres.map(item => 
+          (<p>{item}</p>))}
       
      
     </div>
